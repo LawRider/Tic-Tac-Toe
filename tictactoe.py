7cc8ca87e8db2e2ee19e0bc498a9e123cfa72dcd
Oleksandr <9040602+LawRider@users.noreply.github.com>
2020-11-15 01:26:30 +0200

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -14,48 +14,39 @@ def analyze_field(cells):
 
 def three_in_row(field_list):
     win_list = [['X', 'X', 'X'], ['O', 'O', 'O']]
-    count = 0
-    for item in field_list:
-        if item in win_list:
-            count += 1
-    if count == 1:
-        if win_list[0] in field_list:
-            print('X wins')
-        else:
-            print('O wins')
-        exit()
+    for item in win_list:
+        if item in field_list:
+            if item[0][0] == 'X':
+                print('X wins')
+            else:
+                print('O wins')
+            exit()
 
 
 def next_step(cells, coord):
     row, col = coord.split()
     if not (row.isdigit() or col.isdigit()):
         print("You should enter numbers!")
-    elif int(row) not in range(1, 4) or int(col) not in range(1, 4):
+    elif (int(row) or int(col)) not in range(1, 4):
         print("Coordinates should be from 1 to 3!")
     else:
-        row = int(row)
-        col = int(col)
-        index = (row - 1) + ((3 - col) * 3)
-        if cells[index] in ('X', 'O'):
+        index = (int(row) - 1) + ((3 - int(col)) * 3)
+        if cells[index] is not ' ':
             print("This cell is occupied! Choose another one!")
-
         elif cells.count == 0:
             cells[index] = 'X'
         else:
-            if cells.count('X') > cells.count('O'):
-                cells[index] = 'O'
-            else:
-                cells[index] = 'X'
+            cells[index] = ('O' if cells.count('X') > cells.count('O') else 'X')
 
 
-cells_field = list(' ' * 9)
-print_field(cells_field)
-while cells_field.count('O') + cells_field.count('X') < 9:
-    step_input = input("Enter the coordinates: ")
-    next_step(cells_field, step_input)
+if __name__ == '__main__':
+    cells_field = list(' ' * 9)
     print_field(cells_field)
-    three_in_row(analyze_field(cells_field))
-else:
-    print('Draw')
-    exit()
-
+    while cells_field.count(' ') > 0:
+        step_input = input("Enter the coordinates: ")
+        next_step(cells_field, step_input)
+        print_field(cells_field)
+        three_in_row(analyze_field(cells_field))
+    else:
+        print('Draw')
+        exit()
